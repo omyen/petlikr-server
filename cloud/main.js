093daa7e081ce5615610e65ba977170a965d83c9
Tristan <tristan@Ithaca.local>
2016-08-01 18:21:23 +1000

    added s3 signing cloud code

---
+++ cloud/main.js
@@ -0,0 +1,32 @@
+const aws = require('aws-sdk');
+var log = require('loglevel');
+
+log.setLevel('debug');
+
+const S3_BUCKET = process.env.S3_BUCKET;
+
+Parse.Cloud.define('signS3', function(req, res) {
+	const s3 = new aws.S3();
+	const fileName = req.params.fileName;
+	const fileType = req.params.fileType;
+	const s3Params = {
+		Bucket: S3_BUCKET,
+		Key: fileName,
+		Expires: 60,
+		ContentType: fileType,
+		ACL: 'public-read'
+	};
+
+	s3.getSignedUrl('putObject', s3Params, (err, data) => {
+		if(err){
+			log.error(err);
+			return res.error();
+		}
+		const returnData = {
+			signedRequest: data,
+			url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+		};
+		res.success(JSON.stringify(returnData));
+	});
+	
+});
