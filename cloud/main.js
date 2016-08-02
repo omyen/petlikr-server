11ed8afb311ab0a94df9c147a7f56bfa1180cc8e
Tristan <tristan@Ithaca.local>
2016-08-02 19:55:34 +1000

    new signer

--- cloud/main.js
+++ cloud/main.js
@@ -6,33 +6,28 @@ log.setLevel('debug');
 const S3_BUCKET = process.env.S3_BUCKET;
 
 Parse.Cloud.define('signS3', function(req, res) {
-	var AWS = require('aws-sdk');
-	var s3 = new AWS.S3({params: {Bucket: process.env.S3_BUCKET, Key: process.env.S3_ACCESS_KEY}});
-	s3.getSignedUrl('getObject', {ACL: 'public-read'}, function (err, url) {
-  		if(err){
+	const s3 = new aws.S3();
+	const fileName = req.params.fileName;
+	const s3Params = {
+		Bucket: S3_BUCKET,
+		Key: process.env.S3_ACCESS_KEY,
+		Expires: 60,
+		ContentEncoding: 'base64',
+    	ContentType: 'image/jpeg',
+		ACL: 'public-read'
+	};
+
+	s3.getSignedUrl('putObject', s3Params, (err, data) => {
+		if(err){
 			log.error(err);
 			return res.error();
 		}
-		res.success(url);
+		const returnData = {
+			signedRequest: data,
+			url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+		};
+		res.success(JSON.stringify(returnData));
 	});
-	// const s3 = new aws.S3();
-	// const fileName = req.params.fileName;
-	// const s3Params = {
-	// 	Bucket: S3_BUCKET,
-	// 	Key: fileName,
-	// 	Expires: 60,
-	// 	ContentEncoding: 'base64',
- //    	ContentType: 'image/jpeg',
-	// 	ACL: 'public-read'
-	// };
-
-	// s3.getSignedUrl('putObject', s3Params, (err, data) => {
-	// 	if(err){
-	// 		log.error(err);
-	// 		return res.error();
-	// 	}
-	// 	res.success(data);
-	// });
 	
 });
 
